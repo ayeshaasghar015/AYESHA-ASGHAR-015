ae7b489b7ab7eecf3437a5f8642dd88a6126567e
ayeshaasghar015 <ayeshaasghar023@gmail.com>
2025-02-27 13:31:11 +0500

    ayesha asghar activity lab

---
+++ Programming Pg74 activities.py
@@ -0,0 +1,31 @@
+#Activity 1 and 2
+n=input ("Enter a number ")
+if int (n) %2==0: print ("The given number is an even number")
+else:
+    print("The given number is a odd number")
+
+#Activity 3
+a=input ("Enter first value ")
+
+b=input ("Enter second value ")
+
+if int (a)>int (b):
+    print(a," is greater") 
+elif int (b)>int (a):
+ print (b," is greater")
+else:
+    print ("Both are equal values")
+
+#Activity 4
+
+c=input ("Enter first value ") 
+d=input ("Enter second value ")
+
+if int (c)>int (d): print(c," is greater")
+
+else:
+
+    if int (d)>int (b):
+        print (d," is greater") 
+    else:
+        print ("Both are equal values")
