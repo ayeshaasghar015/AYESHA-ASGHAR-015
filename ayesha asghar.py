cfd524ce34b4074b7fb3b6789e3a76830fab99b9
ayeshaasghar015 <ayeshaasghar023@gmail.com>
2025-02-27 12:34:44 +0500

    assignment no. 1

---
+++ ayesha asghar.py
@@ -0,0 +1,39 @@
+Python 3.4.3 (v3.4.3:9b73f1c3e601, Feb 24 2015, 22:44:40) [MSC v.1600 64 bit (AMD64)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> # Display several numbers
+>>> print(12,13,14,15)
+12 13 14 15
+>>> # Solve and show the summation of 64=32
+>>> print("64+32=",64+32)
+64+32= 96
+>>> # Solve and show summation using variables
+>>> x=64
+>>> y=32
+>>> sum=x+y
+>>> print("sum=",sum)
+sum= 96
+>>> # Print the word 'Lucky' inside a variable
+>>> s="Lucky"
+>>> print(s)
+Lucky
+>>> #print today's date in the foprmat"Today is 2/2/2016"
+>>> day=2
+>>> month=2
+>>> year=2025
+>>> print("Today is",2,"/",2"/",2025)
+SyntaxError: invalid syntax
+>>> print(Today is",2,"/",2,"/",2025)
+      
+SyntaxError: EOL while scanning string literal
+>>> print"Today is",2,"/",2,"/",2025)
+SyntaxError: invalid syntax
+>>> print("Today is",2,"/", 2,"/",2025)
+Today is 2 / 2 / 2025
+>>> # Ask user for phone number and display it
+>>> phone_number=
+SyntaxError: invalid syntax
+>>> phone_number=input("03040484506")
+03040484506
+>>> print("Enter your phone number:",phone_number)
+Enter your phone number: 
+>>> 
